074b7dba9fc0492459546bc6b890c128b143c2a9
Ahmed Bekr <ahmedabobakr2972004@gmail.com>
2026-08-25 01:28:49 +0300

    first commit

--- back/index.js
+++ back/index.js
@@ -3,9 +3,9 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 const helmet = require("helmet");
-const connectDB = require("./src/config/db");
-const apiRouter = require("./src/routes");
-const errorHandler = require("./src/middlewares/error.middleware");
+const connectDB = require("./config/db");
+const apiRouter = require("./routes");
+const errorHandler = require("./middlewares/error.middleware");
 
 const app = express();
 const PORT = process.env.PORT || 3001;
