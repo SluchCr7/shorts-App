551f56b2c6bf8a5abe9ba2018111d351e6467816
Ahmed Bekr <ahmedabobakr2972004@gmail.com>
2026-08-25 18:33:52 +0300

    front end edits

--- front2/app/layout.tsx
+++ front2/app/layout.tsx
@@ -32,7 +32,7 @@ export default function RootLayout({
     <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased dark`}>
       <body className="min-h-full flex flex-col bg-[var(--bg-primary)] text-[var(--text-primary)] transition-colors pb-16 md:pb-0">
         <ReduxProvider>
-          <MobileNav />
+          {/* <MobileNav /> */}
           <div className="flex flex-1 w-full min-h-screen">
             <Sidebar />
             <main className="flex-1 w-full min-w-0">{children}</main>
